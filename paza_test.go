412c672e2d83984ba06c4381bdfd0099f70bc1fd
reus <reusee@gmail.com>
2015-01-25 16:59:55 +0800

    add regular expression parser test

--- paza_test.go
+++ paza_test.go
@@ -6,9 +6,7 @@ func TestAll(t *testing.T) {
 	set := NewSet()
 	set.Add("a", set.Regex(`a`))
 	set.Add("+", set.Regex(`\+`))
-	// direct recursive
 	set.AddRec("expr", set.OrdChoice(set.Concat("expr", "+", "a"), "a"))
-	// TODO indirect recursive
 
 	cases := []struct {
 		text   []byte
@@ -77,6 +75,61 @@ func TestCalc(t *testing.T) {
 		{[]byte("(1)/1**(3-2)"), "expr", true, 5},
 		{[]byte("*(1)/1**(3-2)"), "expr", false, 0},
 		{[]byte(""), "expr", false, 0},
+	}
+
+	for _, c := range cases {
+		input := NewInput(c.text)
+		ok, l := set.Call(c.parser, input, 0)
+		if c.ok != ok || c.length != l {
+			t.Fatalf("%v", c)
+		}
+	}
+}
+
+func TestRegex(t *testing.T) {
+	/*
+		<RE>	::=	<RE> "|" <simple-RE> | <simple-RE>
+		<simple-RE>	::=	<simple-RE> <basic-RE> | <basic-RE>
+		<basic-RE>	::=	<elementary-RE> "*" | <elementary-RE> "+" | <elementary-RE>
+		<elementary-RE>	::=	"(" <RE> ")" | "." | "$" | [a-zA-Z0-9]
+	*/
+	set := NewSet()
+	set.AddRec("re", set.OrdChoice(
+		set.Concat("re", set.Rune('|'), "simple-re"),
+		"simple-re",
+	))
+	set.AddRec("simple-re", set.OrdChoice(
+		set.Concat("simple-re", "basic-re"),
+		"basic-re",
+	))
+	set.Add("basic-re", set.OrdChoice(
+		set.Concat("elementary-re", set.Rune('*')),
+		set.Concat("elementary-re", set.Rune('+')),
+		"elementary-re",
+	))
+	set.Add("elementary-re", set.OrdChoice(
+		set.Concat(set.Rune('('), "re", set.Rune(')')),
+		set.Rune('.'),
+		set.Rune('$'),
+		set.Regex(`[a-zA-Z0-9]`),
+	))
+
+	cases := []struct {
+		text   []byte
+		parser string
+		ok     bool
+		length int
+	}{
+		{[]byte(""), "re", false, 0},
+		{[]byte("a"), "re", true, 1},
+		{[]byte("a*"), "re", true, 2},
+		{[]byte("a.*"), "re", true, 3},
+		{[]byte("a(.*)"), "re", true, 5},
+		{[]byte("a(.*)+"), "re", true, 6},
+		{[]byte("a(.*)+$"), "re", true, 7},
+		{[]byte("a(.*)+$b+"), "re", true, 9},
+		{[]byte("a(.*)+$|b+"), "re", true, 10},
+		{[]byte("a(.*)+$|*b+"), "re", true, 7},
 	}
 
 	for _, c := range cases {
